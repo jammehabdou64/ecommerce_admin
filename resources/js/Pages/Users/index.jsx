b50c65c709156914c430499594faf44c28c5746b
Abdou Jammeh <jammehabdou64@gmail.com>
2024-11-08 12:03:14 +0000

    add app container component

--- resources/js/Pages/Users/index.jsx
+++ resources/js/Pages/Users/index.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Table from "~/Components/Table";
-import TableHeader from "~/Components/TableHeader";
-// import Spinner from "~/Components/Spinner";
 import { deleteApi, getApi } from "~/Api";
 import Layout from "~/Components/Layout";
 import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import AppContainer from "~/Components/AppContainer";
 
 const Users = () => {
   const headers = [
@@ -52,57 +51,53 @@ const Users = () => {
       {loading ? (
         <h1>Loading....</h1>
       ) : (
-        <Table
-          title={"users"}
-          buttonName={"add user"}
-          pathTo={"/users/create"}
-          data={headers}
-        >
-          {/* <TableHeader  /> */}
-          <tbody className="bg-white divide-y divide-gray-200">
-            {users.data.map((user, index) => (
-              <tr key={index}>
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  {index + 1}
-                </td>
+        <AppContainer>
+          <Table columns={headers}>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {users.data.map((user, index) => (
+                <tr key={index}>
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    {index + 1}
+                  </td>
 
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  {user.name}
-                </td>
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  {user.email}
-                </td>
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  {user.primary_phone}
-                </td>
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  {user.secondary_phone}
-                </td>
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  {user.role}
-                </td>
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  {user.created_at}
-                </td>
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    {user.name}
+                  </td>
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    {user.email}
+                  </td>
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    {user.primary_phone}
+                  </td>
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    {user.secondary_phone}
+                  </td>
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    {user.role}
+                  </td>
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    {user.created_at}
+                  </td>
 
-                <td className="px-6 py-2 mr-2 text-sm leading-5 border">
-                  <div className="flex ">
-                    <Link to={"/users/edit/" + user.id}>
-                      <PencilIcon
-                        className="text-blue-500 w-6 cursor-pointer mx-3"
-                        title="edit"
+                  <td className="px-6 py-2 mr-2 text-sm leading-5 border">
+                    <div className="flex ">
+                      <Link to={"/users/edit/" + user.id}>
+                        <PencilIcon
+                          className="text-blue-500 w-6 cursor-pointer mx-3"
+                          title="edit"
+                        />
+                      </Link>
+                      <TrashIcon
+                        className="text-red-500 w-6 cursor-pointer mx-3"
+                        onClick={() => deleteUser(user.id)}
                       />
-                    </Link>
-                    <TrashIcon
-                      className="text-red-500 w-6 cursor-pointer mx-3"
-                      onClick={() => deleteUser(user.id)}
-                    />
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </AppContainer>
       )}
     </Layout>
   );
